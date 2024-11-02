a3d7904c6597b47457ac8f4066df3c9841e25868
Richard Otvos <42136361+naismith1991@users.noreply.github.com>
2024-11-02 10:06:30 +0100

    Initial commit

---
+++ Classes/Auto.py
@@ -0,0 +1,6 @@
+class Auto:
+    def __init__(self, rendszam, tipus, berletiDij):
+        self.rendszam = rendszam
+        self.tipus = tipus
+        self.berletiDij = berletiDij
+        self.szabad = True
